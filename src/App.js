a06e8db2ee488435706e4084b0f1ebaf66b71423
mohak852 <ajayshri2011@gmail.com>
2020-06-07 21:17:37 +0530

    custom css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React,{useState,useEffect} from 'react';
 import {Container ,Row,Col} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Axios from "axios";
+import './styles/app.css';
 import "./App.css"
 import MyCard from "./Card"
+import NavBar from './Navbar';
 function App() {
   const [details,setDetails] = useState({});
   const fetchDetails = async ()=>{
   const {data} = await Axios.get('https://randomuser.me/api/');
-
+  console.log(data);
+  
   const details = data.results[0]
   console.log("Deatils: " + details);
   setDetails(details);
@@ -20,13 +23,8 @@ function App() {
   
   return (
     <div className="App">
-      <Container fluid className="p-4 bg-primary App">
-        <Row>
-          <Col md={4} className="offset-md-4 mt-4">
-            <MyCard details={details}/>
-            </Col>
-        </Row>
-      </Container>
+      <NavBar/>
+      <MyCard details={details} />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,42 +1,60 @@
 import React from "react";
-import {Card,Container,CardBody,CardTitle,CardText, Row} from "reactstrap";
-import {FaEnvelope,FaMapMarkedAlt,FaPhone,FaPortrait} from "react-icons/fa"
 
 
 const MyCard = ({details})=>{
     return(
-        <Card>
-            <CardBody className="text-center">
-                <img 
-                height="150"
-                width="150"
-                className="rounded-circle img-thumbnail border-danger"
-                src={details.picture?.large}
-                />
-                <CardTitle className="text-primary">
-                    <Row>
-                        <h1><FaPortrait className="pr-3"/></h1>
-                        <h1>
-                            <span className="pr-2">{details.name?.title}</span>
-                            <span className="pr-2">{details.name?.first}</span>
-                            <span>{details.name?.last}</span>
-                        </h1>
-                    </Row>
-                </CardTitle>
-                <CardText className="text-info">
-                    <Row>
-                    <h1><FaMapMarkedAlt className="pr-2"/></h1>
-                        <h1>{details.location?.city}</h1>
-                    </Row>
-                    <Row><h1><FaEnvelope className="pr-2"/></h1>
-                    <h2>{details.email}</h2>
-                    </Row>
-                </CardText>
-            </CardBody>
-        </Card>
+        <div>
+            <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"></link>
+            <section class="text-gray-700 body-font">
+                <div class="container px-5 py-24 mx-auto flex flex-wrap">
+                    <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+                        <img alt="feature" class="object-cover object-center h-50 w-150" src={details.picture?.large} />
+                    </div>
+                    <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+                        <div class="flex flex-col mb-10 lg:items-start items-center">
+                            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                                </svg>
+                            </div>
+                            <div class="flex-grow">
+                                
+                                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Name</h2>
+                                    <p class="leading-relaxed text-base">{details.name?.title} {details.name?.first} {details.name?.last}</p>
+                                
+                            </div>
+                        </div>
+                        <div class="flex flex-col mb-10 lg:items-start items-center">
+                            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <circle cx="6" cy="6" r="3"></circle>
+                                    <circle cx="6" cy="18" r="3"></circle>
+                                    <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                                </svg>
+                            </div>
+                            <div class="flex-grow">
+    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Email</h2>
+                                <p class="leading-relaxed text-base">{details.email}</p>
+                            </div>
+                        </div>
+                        <div class="flex flex-col mb-10 lg:items-start items-center">
+                            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                            </div>
+                            <div class="flex-grow">
+                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3 .p-20">PhoneNumber</h2>
+    <p class="leading-relaxed text-base">{details.phone}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
-
 
 
 export default MyCard;
